bb24018726b9fc12285e58adf48a5bafc42299ae
Danil Nizamov <danil.nizamov@codesignal.com>
2022-05-12 16:38:25 +0300

    fixed

--- rest/settings.py
+++ rest/settings.py
@@ -140,9 +140,9 @@ FIXTURE_DIRS = [
     "main/fixtures",
 ]
 
-importdjango_heroku
+import django_heroku
 django_heroku.settings(locals())
 
-importdj_database_url
+import dj_database_url
 db_from_env= dj_database_url.config(conn_max_age=500) 
 DATABASES['default'].update(db_from_env) 
